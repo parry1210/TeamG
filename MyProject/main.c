83e89fabc88b3a640d73a68fb26a59b7252aa940
parry1210 <92861680+parry1210@users.noreply.github.com>
2021-11-01 09:56:23 +0000

    Update main.c

--- MyProject/main.c
+++ MyProject/main.c
@@ -12,6 +12,12 @@
 
 #include "sensors/proximity.h"
 
+#include "epuck1x/uart/e_uart_char.h"
+#include "stdio.h"
+#include "serial_comm.h"
+
+
+
 messagebus_t bus;
 MUTEX_DECL(bus_lock);
 CONDVAR_DECL(bus_condvar);
@@ -19,44 +25,55 @@ CONDVAR_DECL(bus_condvar);
 
 int main(void)
 {
+	    halInit();
+		chSysInit();
+		mpu_init();
+		messagebus_init(&bus,&bus_lock,&bus_condvar);//proximity
 
-    halInit();
-    chSysInit();
-    mpu_init();
-    
-    messagebus_init(&bus,&bus_lock,&bus_condvar);
-    
-    motors_init();
-    
-    proximity_start();
-    calibrate_ir();
+		motors_init();
 
+		proximity_start();
+		calibrate_ir();
 
-    /* Infinite loop. */
-    while (1) {
-    	//waits 1 second
-        chThdSleepMilliseconds(100);
-        
-        left_motor_set_speed(500);
-        right_motor_set_speed(500);
-        
-        for(int i=0;i<8;i++){
-            prox_values[i]=get_prox(i);
-        }
-        
-        if(prox_values[0]==106||prox_values[1]==151||prox_values[2]==2147){
-            chThdSleepMilliseconds(100);
-            left_motor_set_speed(300);
-            right_motor_set_speed(-300);
-        else if(prox_values[7]==330||prox_values[6]==3658||prox_values[5]==59){
-            chThdSleepMilliseconds(100);
-            left_motor_set_speed(-300);
-            right_motor_set_speed(300);
-        else if(prox_values[3]==1||prox_values[4]==1{
-            chThdSleepMilliseconds(100);
-            left_motor_set_speed(500);
-            right_motor_set_speed(500);
-    }
+		serial_start();
+		void e_send_uart1_char(const char * buff, int buff_len);
+
+		/* Infinite loop. */
+		while (1) {
+	    	//waits 1 second
+	    	chThdSleepMilliseconds(50);
+	    	left_motor_set_speed(500);
+	    	right_motor_set_speed(500);
+
+	    	int str_length;
+	    	char str[100];
+	    	int prox_values[8];
+
+	    	for(int i=0;i<8;i++)
+	    	{
+	          	prox_values[i]=get_calibrated_prox(i);
+	          	str_length=sprintf(str,"prox_values %d is %d\n",i,prox_values[i]);
+	          	e_send_uart1_char(str,str_length);
+	          	chThdSleepMilliseconds(50);
+	    	}
+
+	    	if (prox_values[0]>50&&prox_values[7]>50){
+	    		bool turn_left=(prox_values[1]+prox_values[2])>(prox_values[6]+prox_values[5]);
+	    	    bool turn_right=(prox_values[1]+prox_values[2])<(prox_values[6]+prox_values[5]);
+	    	    if(turn_left){
+	    	    	left_motor_set_speed(-1000);
+	        	    right_motor_set_speed(1000);
+	        	    chThdSleepMilliseconds(500);
+	    	    }
+	    	    if(turn_right){
+	    	    	left_motor_set_speed(1000);
+	        	    right_motor_set_speed(-1000);
+	        	    chThdSleepMilliseconds(500);
+	    	    }
+	        }
+
+		 }
+
 }
 
 #define STACK_CHK_GUARD 0xe2dee396
